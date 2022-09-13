20a1aa6583a37214145f6efcdc0c2d81506a8dbf
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2022-09-13 21:36:17 +0300

    feat: added dto validation, debug config, added e2e and unit test, update package

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,14 +1,45 @@
+import { ReviewService } from './review.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { DocumentType } from '@typegoose/typegoose/lib/types';
 import { ReviewModel } from './review.model';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @UsePipes(new ValidationPipe())
   @Post('create')
-  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+  async create(
+    @Body() dto: CreateReviewDto,
+  ): Promise<DocumentType<ReviewModel>> {
+    return this.reviewService.create(dto);
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string): Promise<void> {
+    const deletedDoc = await this.reviewService.delete(id);
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+  }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {}
+  async getByProduct(
+    @Param('productId') productId: string,
+  ): Promise<DocumentType<ReviewModel>[]> {
+    return this.reviewService.findByProductId(productId);
+  }
 }
